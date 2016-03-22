613572fbeefa00048b458783461b03e8069e00fc
Heley Chen <zccz14@outlook.com>
2016-03-22 23:22:45 +0800

    对用户简单建模

--- models/user.js
+++ models/user.js
@@ -2,9 +2,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = mongoose.model('user', new Schema({
-    username : {
-        type : String,
-        required : 'miss username',
-        unique : true,
-    }
+    username: {
+        type: String,
+        required: 'miss username',
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: 'miss password',
+    },
+    nickname: {
+        type: String,
+        required: 'miss nickname',
+        unique: true,
+    },
+    // real name
+    name: {
+        type: String,
+    },
+    // personal description
+    description: {
+        type: String,
+    },
 }));
